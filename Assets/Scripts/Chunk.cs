b83336720d06b8217f784346630bfac42f9dbe1c
dbaric99 <damjana.baric1999@gmail.com>
2023-06-12 22:07:13 +0200

    Refactored code into functions

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -7,13 +7,22 @@ public class Chunk : MonoBehaviour
     public MeshRenderer meshRenderer;
     public MeshFilter meshFilter;
     const int SIDES_NUMBER = 6;
+
+    int vertexIndex = 0;
+    List<Vector3> vertices = new List<Vector3>();
+    List<int> triangles = new List<int>();
+    List<Vector2> uvs = new List<Vector2>();
+
     void Start()
     {
-        int vertexIndex = 0;
-        List<Vector3> vertices = new List<Vector3>();
-        List<int> triangles = new List<int>();
-        List<Vector2> uvs = new List<Vector2>();
 
+        AddVoxelToChunk();
+        CreateMesh();
+
+    }
+
+    void AddVoxelToChunk()
+    {
         for (int j = 0; j < SIDES_NUMBER; j++)
         {
             for (int i = 0; i < SIDES_NUMBER; i++)
@@ -21,11 +30,14 @@ public class Chunk : MonoBehaviour
                 int triangleIndex = VoxelData.voxelTriangles[j, i];
                 vertices.Add(VoxelData.voxelVertices[triangleIndex]);
                 triangles.Add(vertexIndex);
-                uvs.Add(Vector2.zero);
+                uvs.Add(VoxelData.voxelUvs[i]);
                 vertexIndex++;
             }
         }
+    }
 
+    void CreateMesh()
+    {
         Mesh mesh = new Mesh();
         mesh.vertices = vertices.ToArray();
         mesh.triangles = triangles.ToArray();
--- Assets/Scripts/VoxelData.cs
+++ Assets/Scripts/VoxelData.cs
@@ -20,9 +20,18 @@ public static class VoxelData
     public static readonly int[,] voxelTriangles = new int[6, 6] {
         {0, 3, 1, 1, 3, 2}, //Back Square
         {5, 6, 4, 4, 6, 7}, //Front Square
-        {3,7,2,2,7,6}, //Top Square
+        {3, 7, 2, 2, 7, 6}, //Top Square
         {1, 5, 0, 0, 5, 4}, //Bottom Square
         {4, 7, 0, 0, 7, 3}, //Left Square
         {1, 2, 5, 5, 2, 6} //Right Square
+    };
+
+    public static readonly Vector2[] voxelUvs = new Vector2[6] {
+        new Vector2(0.0f, 0.0f),
+        new Vector2(0.0f, 1.0f),
+        new Vector2(1.0f, 0.0f),
+        new Vector2(1.0f, 0.0f),
+        new Vector2(0.0f, 1.0f),
+        new Vector2(1.0f, 1.0f)
     };
 }
